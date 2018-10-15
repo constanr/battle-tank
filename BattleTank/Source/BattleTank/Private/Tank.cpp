f857e2e9fbe3b296566c4605c824e8e76c07f96f
Constantino Román Gómez <constanromangomez@hotmail.com>
2018-10-15 21:10:35 +0200

    176-178: Added pointer protection, temporal logging and modified setting of component in BP

--- BattleTank/Source/BattleTank/Private/Tank.cpp
+++ BattleTank/Source/BattleTank/Private/Tank.cpp
@@ -9,6 +9,7 @@
 // Sets default values
 ATank::ATank()
 {
+	UE_LOG(LogTemp, Warning, TEXT("NINONINO: ATank constructor"))
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = false;
 }
@@ -16,12 +17,13 @@ ATank::ATank()
 // Called when the game starts or when spawned
 void ATank::BeginPlay()
 {
-	Super::BeginPlay();
-	
+	UE_LOG(LogTemp, Warning, TEXT("NINONINO: ATank BeginPlay"))
+	Super::BeginPlay();	
 }
 
 void ATank::AimAt(FVector HitLocation)
 {
+	if (!TankAimingComponent) { return; }
 	TankAimingComponent->AimAt(HitLocation, LaunchSpeed);
 }
 
